74c669f626e58b218c7a5db314ec4f68ac3a7bcb
anny og <sg2422732gmail.com>
2024-02-24 12:28:20 +0700

    Inital commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace PR2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Целые числа от 100 до 200, кратные трем:");
+            Console.WriteLine("========================================");
+            for (int i = 100; i <= 200; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine($"{i}");
+                }
+            }
+        }
+    }
+}
